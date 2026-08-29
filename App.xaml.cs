d59e64112be9f0d98a3d61c8a575422216e7d332
AbnerStephen <AbnerStephen@DESKTOP-M4LI4A7>
2026-08-30 00:32:24 +0800

    添加项目文件。

---
+++ App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace mewu_ai_Assistant
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
